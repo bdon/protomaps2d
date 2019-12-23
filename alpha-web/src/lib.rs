50a1b1c62909c67f2d28f2d9135be5b6a2c38b68
Brandon Liu <bdon@bdon.org>
2019-12-22 22:55:47 -0500

    better handle dom elements going away

--- alpha-web/src/lib.rs
+++ alpha-web/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::{console, window, HtmlCanvasElement};
+use web_sys::{window, HtmlCanvasElement};
 use piet_web::WebRenderContext;
 use protomaps_alpha::render_tile;
 
@@ -11,28 +11,38 @@ extern crate console_error_panic_hook;
 #[wasm_bindgen]
 pub fn wasm_render_tile(tile_id: &str,buf: Vec<u8>) {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
-
-    let window = window().unwrap();
-    let handle = window
-        .document()
-        .unwrap()
-        .get_element_by_id(tile_id);
-
-    if handle == None {
+    let window_opt = window();
+    if window_opt.is_none() {
+        return;
+    }
+    let window = window_opt.unwrap();
+    let document = window.document();
+    if document.is_none() {
+        return;
+    }
+    let handle = document.unwrap().get_element_by_id(tile_id);
+    if handle.is_none() {
+        return;
+    }
+    let canvas = handle.unwrap().dyn_into::<HtmlCanvasElement>();
+    if canvas.is_err() {
+        return;
+    }
+    let c1 = canvas.unwrap().get_context("2d");
+    if c1.is_err() {
+        return;
+    }
+    let c2 = c1.unwrap();
+    if c2.is_none() {
+        return;
+    }
+    let c3 = c2.unwrap().dyn_into::<web_sys::CanvasRenderingContext2d>();
+    if c3.is_err() {
         return;
     }
 
-    let canvas = handle.unwrap()
-        .dyn_into::<HtmlCanvasElement>()
-        .unwrap();
-    let mut context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
-
-    let mut rc = WebRenderContext::new(&mut context, &window);
+    let mut c4 = c3.unwrap();
+    let mut rc = WebRenderContext::new(&mut c4, &window);
 
     render_tile(&mut rc,&buf);
 }
