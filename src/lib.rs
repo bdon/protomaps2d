05f085abee5b1db2cd9af821e1704af3af865fc4
Brandon Liu <bdon@bdon.org>
2020-05-24 21:00:06 +0800

    colors, poi rendering

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ extern crate log;
 
 pub fn small_size(zoom:u32) -> f64 {
     if zoom < 8 {
-        return 24.0;
+        return 30.0;
     }
     return 36.0;
 }
@@ -35,16 +35,16 @@ pub fn highway_size(zoom:u32) -> (f64,f64) {
 }
 
 pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
-    rc.clear(Color::rgba8(0xFF,0xFC,0xEB,0xFF));
+    rc.clear(Color::rgba8(0xF6,0xE7,0xD4,0xFF));
     let text = rc.solid_brush(Color::rgba8(0x44, 0x44, 0x44, 0xFF));
     let text_halo = rc.solid_brush(Color::rgba8(0xFF, 0xFF, 0xFF, 0xFF));
     let mid_gray = rc.solid_brush(Color::rgba8(0x55, 0x55, 0x55, 0xFF));
 
-    let road_0 = rc.solid_brush(Color::rgba8(0xE8,0x92,0xA2,0xFF));
-    let road_0_buf = rc.solid_brush(Color::rgba8(0xE4,0x6B,0x8D,0xFF));
+    let road_0 = rc.solid_brush(Color::rgba8(0xFF,0xFF,0xFF,0xFF));
+    //let road_0_buf = rc.solid_brush(Color::rgba8(0xE4,0x6B,0x8D,0xFF));
 
-    let park = rc.solid_brush(Color::rgba8(0xD2, 0xFE, 0xC6, 0xFF));
-    let water = rc.solid_brush(Color::rgba8(0xCD,0xEC,0xFF,0xFF));
+    let park = rc.solid_brush(Color::rgba8(0xBB, 0xDB, 0xC4, 0xFF));
+    let water = rc.solid_brush(Color::rgba8(0xA5,0xBC,0xCB,0xFF));
     let buildings = rc.solid_brush(Color::rgba8(0xCC,0xCC,0xCC,0xFF));
 
     let mut reader = BytesReader::from_bytes(&buf);
@@ -54,6 +54,16 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
 
     for layer in &tile.layers {
         if layer.name == "landuse" {
+            for feature in &layer.features {
+                if feature.type_pb != GeomType::POLYGON {
+                    continue
+                }
+                rc.fill(draw::path(&feature.geometry,layer.extent), &park);
+            }
+        }
+    }
+    for layer in &tile.layers {
+        if layer.name == "natural" {
             for feature in &layer.features {
                 if feature.type_pb != GeomType::POLYGON {
                     continue
@@ -87,8 +97,8 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
             if feature.type_pb != GeomType::LINESTRING {
                 continue
             }
-            let kind_val = taggetstr(layer,feature,"kind");
-            let sort_rank = taggetint(layer,feature,"sort_rank");
+            let kind_val = tile::taggetstr(layer,feature,"kind");
+            let sort_rank = tile::taggetint(layer,feature,"sort_rank");
             if kind_val.is_some() && kind_val.unwrap() == "ferry" {
                 continue
             }
@@ -96,7 +106,7 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
             if kind_val.is_some() && kind_val.unwrap() == "highway" {
                 rds.push((&feature.geometry,sort_rank.unwrap()));
             } else {
-                rc.stroke(draw::path(&feature.geometry,layer.extent), &mid_gray, 1.0);
+                rc.stroke(draw::path(&feature.geometry,layer.extent), &road_0, 1.0);
             }
         }
     };
@@ -106,7 +116,6 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
     for rd in rds {
         let path = draw::path(&rd.0,8192);
         let size = highway_size(zoom);
-        rc.stroke(&path, &road_0_buf, size.1);
         rc.stroke(&path, &road_0, size.0);
     }
 
@@ -121,12 +130,23 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
         }
     }
 
+    for layer in &tile.layers {
+        if layer.name == "admin" {
+            for feature in &layer.features {
+                if feature.type_pb != GeomType::LINESTRING {
+                    continue
+                }
+                rc.stroke(draw::path(&feature.geometry,layer.extent), &mid_gray, 1.0);
+            }
+        }
+    }
+
     let mut collider = label::Collider{bboxes:Vec::new()};
-    let font_size_big = 48.0;
-    let font_big = rc.text().new_font_by_name("Helvetica", font_size_big).build().unwrap();
+    let font_size_big = 20.0;
+    let font_big = rc.text().new_font_by_name("Inter", font_size_big).build().unwrap();
 
     let font_size_small = small_size(zoom);
-    let font_small = rc.text().new_font_by_name("Helvetica", font_size_small).build().unwrap();
+    let font_small = rc.text().new_font_by_name("Inter", font_size_small).build().unwrap();
 
     for layer in &tile.layers {
         if layer.name != "places" {
@@ -138,7 +158,7 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
 
             let nam = tile::taggetstr(layer,feature,"name");
 
-            let kind_val = tile::taggetstr(layer,feature,"kind");
+            let kind_val = tile::taggetstr(layer,feature,"place");
             if nam.is_some() {
                 if kind_val.is_some() && kind_val.unwrap() == "country" {
                     let layout = rc.text().new_text_layout(&font_big, &nam.unwrap()).build().unwrap();
@@ -150,7 +170,7 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
                     }
                     rc.stroke_text(&layout, (cursor_x-layout.width()/2.0,cursor_y), &text_halo,8.0);
                     rc.draw_text(&layout, (cursor_x-layout.width()/2.0,cursor_y), &text);
-                } else if kind_val.is_some() && kind_val.unwrap() == "locality" {
+                } else if kind_val.is_some() && kind_val.unwrap() == "city" {
                     let layout = rc.text().new_text_layout(&font_small, &nam.unwrap()).build().unwrap();
                     if (cursor_y-font_size_small < 0.0) || (cursor_x + layout.width()/2.0 > 2048.0) || (cursor_y > 2048.0) {
                         continue;
@@ -161,6 +181,30 @@ pub fn render_tile(rc:&mut impl RenderContext, buf:&Vec<u8>, zoom:u32) {
                     rc.stroke_text(&layout, (cursor_x,cursor_y), &text_halo,8.0);
                     rc.draw_text(&layout, (cursor_x,cursor_y), &text);
                 }
+            }
+        }
+    } 
+
+    for layer in &tile.layers {
+        if layer.name != "poi" {
+            continue
+        }
+        for feature in &layer.features {
+            let cursor_x = draw::de_zig_zag(layer.extent,feature.geometry[1]);
+            let cursor_y = draw::de_zig_zag(layer.extent,feature.geometry[2]);
+
+            let nam = tile::taggetstr(layer,feature,"name");
+
+            if nam.is_some() {
+                let layout = rc.text().new_text_layout(&font_big, &nam.unwrap()).build().unwrap();
+                if (cursor_y-font_size_big < 0.0) || (cursor_x - layout.width()/2.0 < 0.0) || (cursor_x + layout.width()/2.0 > 2048.0) || (cursor_y > 2048.0) {
+                    continue;
+                }
+                if !collider.add((cursor_x-layout.width()/2.0,cursor_y-font_size_big),(cursor_x+layout.width()/2.0,cursor_y)) {
+                    continue;
+                }
+                rc.stroke_text(&layout, (cursor_x-layout.width()/2.0,cursor_y), &text_halo,4.0);
+                rc.draw_text(&layout, (cursor_x-layout.width()/2.0,cursor_y), &text);
             }
         }
     } 
